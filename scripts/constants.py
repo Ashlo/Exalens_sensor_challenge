39f55c928b0e77259c672029aacbedfcf549756a
Ashutosh Bele <ashutoshbele5@gmail.com>
2023-08-21 21:04:33 +0530

    Implemented MongoDB integration for data storage.

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,3 +1,11 @@
 BROKER="localhost"
 PORT=1883
 TIMEOUT=60
+
+
+# MONGO CONSTANTS
+
+# Connection parameters
+MONGO_URL = "mongodb://rootuser:rootpassword@localhost:27017/"
+DATABASE_NAME = "Sensors"
+COLLECTION_NAME = "tempreature_and_humidty"
--- scripts/subscriber.py
+++ scripts/subscriber.py
@@ -1,6 +1,12 @@
 import paho.mqtt.client as mqtt
 import json
-from constants import BROKER,PORT,TIMEOUT
+from constants import BROKER,PORT,TIMEOUT,MONGO_URL,DATABASE_NAME,COLLECTION_NAME
+from pymongo import MongoClient
+
+# Connect to the MongoDB server
+mongo_client = MongoClient(MONGO_URL)
+db = mongo_client[DATABASE_NAME]
+collection = db[COLLECTION_NAME]
 
 def on_connect(client,userdata,flags,rc):
     try:
@@ -15,8 +21,13 @@ def on_message(client,userdata,msg):
         print(msg.topic,msg.payload.decode('utf-8'))
         data = json.loads(msg.payload)
 
-        #TODO MONGO DB LOGICS Insertion
+        #TODO Mongo_json final structure
+        mongo_json = {
+        "topic": msg.topic,
+        "message": msg.payload.decode()
+        }
 
+        collection.insert_one(mongo_json)
         print(f"Recieved data from {data['sensor_id']:}:{data['value']} at {data['timestamp']}")
     except Exception as e:
         print(e)
