c475683d11a9a471716cdf3b116438d721e913e8
Ashutosh Bele <ashutoshbele5@gmail.com>
2023-08-24 00:18:42 +0530

    Redis Integration

--- scripts/subscriber.py
+++ scripts/subscriber.py
@@ -2,14 +2,36 @@ import paho.mqtt.client as mqtt
 import json
 from constants import BROKER,PORT,TIMEOUT,MONGO_URL,DATABASE_NAME,COLLECTION_NAME
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 from datetime import datetime
+import redis
 
+# Connect to the local Redis instance
+redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, password='eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81')
 #TODO Redis implementation
 
 # Connect to the MongoDB server
 mongo_client = MongoClient(MONGO_URL)
 db = mongo_client[DATABASE_NAME]
 collection = db[COLLECTION_NAME]
+
+
+def jsonify_mongo_data(data):
+    """Converts data containing MongoDB's ObjectId to JSON-serializable format"""
+    if isinstance(data, dict):
+        for k, v in data.items():
+            if isinstance(v, ObjectId):
+                data[k] = str(v)
+            else:
+                jsonify_mongo_data(v)
+    elif isinstance(data, list):
+        for i, v in enumerate(data):
+            if isinstance(v, ObjectId):
+                data[i] = str(v)
+            else:
+                jsonify_mongo_data(v)
+    return data
+
 
 def on_connect(client,userdata,flags,rc):
     try:
@@ -29,6 +51,15 @@ def on_message(client,userdata,msg):
             }
 
         collection.insert_one(mongo_json)
+
+        # Store the latest 10 readings for each sensor in Redis
+        # Redis Lists will be used here
+        # LPUSH adds a value to the front of the list, LTRIM ensures we only keep the latest 10
+        serializable_data = jsonify_mongo_data(mongo_json)
+        redis_key = serializable_data['sensor_id']
+        print(redis_key)
+        redis_client.lpush(redis_key, json.dumps(serializable_data))
+        redis_client.ltrim(redis_key, 0, 9)
     except Exception as e:
         print(e)
 
